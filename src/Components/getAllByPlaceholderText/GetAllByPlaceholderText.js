bb4fc5aa37f069db742a8ffda0b6a6c4d4fb239d
surya6394 <ravinigam0720@gmail.com>
2025-09-09 18:31:29 +0530

    Learned about getAllByLabelText() method in RTL query

---
+++ src/Components/getAllByPlaceholderText/GetAllByPlaceholderText.js
@@ -0,0 +1,27 @@
+export default function GetAllByPlaceholderText(){
+    return(
+        <>
+        
+            <div>
+                <div>
+                    <h1>Get by label text Method: getByLabelText()</h1>
+                    <label htmlFor="username">username</label>
+                    <input id="username" type="text" placeholder="Enter your name" name="GetByLabelText" defaultValue="surya" />
+                </div>
+
+                <div>
+                    <h1>Get by label text Method: getByLabelText()</h1>
+                    <label htmlFor="username">username</label>
+                    <input id="username" type="text" placeholder="Enter your name" name="GetByLabelText" defaultValue="surya" />
+                </div>
+
+                <div>
+                    <h1>Get by label text Method: getByLabelText()</h1>
+                    <label htmlFor="username">username</label>
+                    <input id="username" type="text" placeholder="Enter your name" name="GetByLabelText" defaultValue="surya" />
+                </div>
+            </div>
+        
+        </>
+    )
+}
---
+++ src/Components/getByPlaceholderText/GetByPlaceholderText.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react"
+import GetByPlaceholderText from "./GetByPlaceholderText"
+
+test("Testing placeholder in input box", () => {
+    render(<GetByPlaceholderText />);
+    const inputbox = screen.getByPlaceholderText('Enter your name');
+    expect(inputbox).toBeInTheDocument();
+})
